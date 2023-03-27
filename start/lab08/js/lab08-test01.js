9c965d906137213344f3d9e9a8e25ce3bf1697a6
zgweks <92344996+zgwek@users.noreply.github.com>
2023-03-27 15:57:08 -0400

    calculate bill total

--- start/lab08/js/lab08-test01.js
+++ start/lab08/js/lab08-test01.js
@@ -1,2 +1,15 @@
 /* add code below this */
+// #1. Provide a prompt to the user to enter a bill total.
+let total = prompt("What is the bill total");
+//2. Convert this user input to a number (don’t worry about error handling for nonnumbers).
+total = Number(total);
+//3. Calculate the tip amount assuming 10% (simply multiply the user input by 0.1). 
+//Use a const to define the 10% tip percentage.
+const tip = 0.1
+let totalTip = total * tip;
+//4. Display the bill total and tip amount on the same console output line, for 
+//example
+console.log("Bill "  + total + " Tip " + totalTip);
+
+
 
