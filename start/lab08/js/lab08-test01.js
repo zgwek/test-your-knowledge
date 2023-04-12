b22aa86821c4d063592b002a68a2afde8c61fbd8
zgweks <92344996+zgwek@users.noreply.github.com>
2023-04-11 23:39:59 -0400

    add shadow box , border, and textcontent

--- start/lab08/js/lab08-test01.js
+++ start/lab08/js/lab08-test01.js
@@ -2,7 +2,9 @@
 // #1. Provide a prompt to the user to enter a bill total.
 let total = prompt("What is the bill total");
 //2. Convert this user input to a number (don’t worry about error handling for nonnumbers).
-total = Number(total);
+
+let numTotal = Number(total);
+//if ()
 //3. Calculate the tip amount assuming 10% (simply multiply the user input by 0.1). 
 //Use a const to define the 10% tip percentage.
 const tip = 0.1
--- start/lab09/js/lab09-test01.js
+++ start/lab09/js/lab09-test01.js
@@ -1,2 +1,36 @@
 /* add code after this comment */
 
+// Use getElementById to add a border via CSS to the <ul> element with the 
+// name "thumb-list".
+
+let list1 = document.getElementById("thumb-list");
+list1.style.border = "solid 1px #CCCCCC ";
+
+// Use querySelector to set the value property of the <textarea> to the textContent 
+// property of the <p> element
+let textarea = document.querySelector("textarea");
+textarea.value = document.querySelector("p").textContent;
+
+// Use querySelectorAll to add a box shadow to each of the <img> elements 
+// within the <ul> element. The CSS property name is box-shadow so the 
+// JavaScript DOM property name will be boxShadow. To see a sample b shadow, look at the example for the box class in lab09-ex01.css. Remember 
+// that you will need to use a loop. The result should look similar to that shown 
+// in Figure 9.5.
+
+
+//const item = document.querySelector("ul img ");
+//item.style.boxShadow= "10px 20px 30px black";
+
+
+const items = document.querySelectorAll("ul img ");
+  for (let i of items) {
+    i.style.boxShadow = "5px 15px 20px 1px black";
+  }
+
+
+// document.querySelector('#msg').value = document.querySelector('.box fieldset p').textContent;
+
+// const images = document.querySelectorAll('#thumb-list img');
+// for (let img of images) {
+//     img.style.boxShadow = "6px 5px 20px 1px rgba(0,0,0,0.22)";
+// }
